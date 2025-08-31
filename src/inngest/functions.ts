312dd091411e5e6a7359f85dd8834135d21bf74d
Shuvo <sfsourov41@gmail.com>
2025-08-31 11:36:57 +0600

    05. AI Job

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
 import { inngest } from "./client";
+import { createAgent, openai } from '@inngest/agent-kit';
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+
+
+const codeAgent = createAgent({
+  name: 'codeAgent',
+  system: 'You are an expert Next.js Developer. You write readable, maintainable code. You write simple Next.js & React snippets',
+  model: openai({model:'gpt-4o'}),
+});
+
+  const { output } = await codeAgent.run(
+  `Write the following snippet: ${event.data.value}`,
+);
+    return { output };
   },
 );
